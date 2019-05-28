41d075c6208f87b447c0704026bd8e922b959174
Algohex Team <algohex@gmail.com>
2019-05-28 22:16:10 +0800

    initial commit

---
+++ algohex-wp-float-fb-sidebar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Floating Facebook Sidebar - by Aloghex
+ * Plugin URI:
+ * Description: Display a Facebook sidebar on your website, work with any public Facebook page
+ * Version: 0.1.0
+ * Author: Algohex Web Developer Team
+ * Author URI:
+ * License: GPL-3.0
+ * Text Domain: alffs
+ */
+
